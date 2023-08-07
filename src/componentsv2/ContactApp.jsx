6fb0a5617bfb3587ee0a35c447849ac2f5e807a2
Filip-Kuta <114823329+Filip-Kuta@users.noreply.github.com>
2023-08-07 19:53:02 +0200

    zminay

--- src/componentsv2/ContactApp.jsx
+++ src/componentsv2/ContactApp.jsx
@@ -4,20 +4,40 @@ import './ContactApp.css';
 
 class ContactApp extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
+    surname: '',
     number: '',
   };
 
+  saveContactsToLocalStorage = (contacts) => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  };
+
+  loadContactsFromLocalStorage = () => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts ? JSON.parse(storedContacts) : [];
+  };
+
+  componentDidMount() {
+    const loadedContacts = this.loadContactsFromLocalStorage();
+    this.setState({ contacts: loadedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      this.saveContactsToLocalStorage(contacts);
+    }
+  }
+
   handleChange = (event) => {
     const { name, value } = event.target;
-    if (name === 'number') {
+    if (name === 'name' || name === 'surname') {
+      const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
+      this.setState({ [name]: capitalizedValue });
+    } else if (name === 'number') {
       const formattedValue = value.replace(/\D/g, '').slice(0, 9);
       const formattedNumber = formattedValue.replace(/(\d{3})(\d{1,3})?(\d{1,3})?/, (_, p1, p2, p3) => {
         let result = p1;
@@ -33,18 +53,22 @@ class ContactApp extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const { name, number, contacts } = this.state;
+    const { name, surname, number, contacts } = this.state;
     const existingContact = contacts.find(
-      (contact) => contact.name.toLowerCase() === name.toLowerCase() && contact.number === number
+      (contact) =>
+        contact.name.toLowerCase() === name.toLowerCase() &&
+        contact.surname.toLowerCase() === surname.toLowerCase() &&
+        contact.number === number
     );
     if (existingContact) {
-      alert('Contact with the same name and number already exists.');
+      alert('Contact with the same name, surname, and number already exists.');
     } else {
       const id = nanoid();
-      const newContact = { id, name, number };
+      const newContact = { id, name, surname, number };
       this.setState({
         contacts: [...contacts, newContact],
         name: '',
+        surname: '',
         number: '',
       });
     }
@@ -61,11 +85,19 @@ class ContactApp extends Component {
     this.setState({ contacts: updatedContacts });
   };
 
-  render() {
-    const { name, number, contacts, filter } = this.state;
+  handleResetContacts = () => {
+    if (window.confirm('Are you sure you want to delete all contacts? This action cannot be undone.')) {
+      this.setState({ contacts: [] });
+    }
+  };
 
-    const filteredContacts = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+  render() {
+    const { name, number, surname, contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+        contact.surname.toLowerCase().includes(filter.toLowerCase())
     );
 
     return (
@@ -73,29 +105,51 @@ class ContactApp extends Component {
         <h1>Phonebook</h1>
 
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleChange}
-          />
+          <div className="form-section">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder='Jan'
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.handleChange}
+            />
+          </div>
 
-          <label htmlFor="number">Phone Number:</label>
-          <input
-            type="text"
-            name="number"
-            title="Phone number must be digits and can start with +"
-            required
-            value={number}
-            onChange={this.handleChange}
-          />
+          <div className="form-section">
+            <label htmlFor='surname'>Surname:</label>
+            <input
+              type="text"
+              id="surname"
+              name="surname"
+              placeholder='Kowalski'
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Surname"
+              required
+              value={surname}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-section">
+            <label htmlFor="number">Phone Number:</label>
+            <input
+              type="text"
+              name="number"
+              placeholder='000-000-000'
+              title="Phone number"
+              required
+              value={number}
+              onChange={this.handleChange}
+            />
+          </div>
 
           <button type="submit">Add Contact</button>
+          
         </form>
 
         <h2>Contacts</h2>
@@ -108,11 +162,14 @@ class ContactApp extends Component {
         <ul>
           {filteredContacts.map((contact) => (
             <li key={contact.id}>
-              {contact.name}: {contact.number}{' '}
+              {contact.name} {contact.surname}: {contact.number}{' '}
               <button onClick={() => this.handleDeleteContact(contact.id)}>Delete</button>
             </li>
           ))}
         </ul>
+        <button className="reset-button" type="button" onClick={this.handleResetContacts}>
+            Reset Contacts
+          </button>
       </div>
     );
   }
